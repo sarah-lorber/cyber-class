7554e385aa5ea08aaa941db43264ba4e6b4c1474
sarah-lorber <sarah.lorber@mymail.champlain.edu>
2026-02-11 14:15:24 -0500

    web scraping

---
+++ webscraping/ScrapingChamplain.ps1
@@ -0,0 +1,36 @@
+﻿function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.43/Courses2026SP.html
+
+$trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+
+$FullTable = @()
+for ($i = 1; $i -lt $trs.length; $i++){
+$tds = $trs[$i].getElementsByTagName("td")
+$Times = $tds[5].innerText.split("-")
+$FullTable += [PSCustomObject]@{"Class Code" = $tds[0].innerText;
+                                "Title" = $tds[1].innerText;
+                                "Days" = $tds[4].innerText;
+                                "Time Start" = $Times[0];
+                                "Time End" = $Times[1];
+                                "Instructor" = $tds[6].innerText;
+                                "Location" = $tds[9].innerText;}
+                                }
+return $FullTable
+}
+
+function daysTranslator($FullTable){
+for($i=0; $i -lt $FullTable.length; $i++) {
+$days = @()
+if($FullTable[$i].Days -ilike "M*"){$days += "Monday"}
+
+if($FullTable[$i].Days -ilike "*T[TWF]*"){$days += "Tuesday"}
+ElseIf($FullTable[$i].Days -ilike "T"){$days += "Tuesday"}
+
+if($FullTable[$i].Days -ilike "*W*"){$days += "Wednesday"}
+if($FullTable[$i].Days -ilike "*TH*"){$days += "Thursday"}
+if($FullTable[$i].Days -ilike "*F*"){$days += "Friday"}
+$FullTable[$i].Days = $days
+}
+return $FullTable
+}
---
+++ webscraping/ScrapingMain.ps1
@@ -0,0 +1,28 @@
+﻿. (Join-Path $PSScriptRoot ScrapingChamplain.ps1)
+
+clear
+
+$tableRecords = gatherClasses
+$tableUpdate = daysTranslator $tableRecords
+#Write-Host ($tableUpdate | Format-Table -AutoSize | Out-String)
+
+#$tableUpdate | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" | 
+#    Where-Object {$_."Instructor" -ilike "Furkan*"}
+
+#$tableUpdate | Where-Object {($_.Location -ilike "FREE 105") -and ($_.days -contains "Wednesday")} |
+#    Sort-Object -Descending "Time Start" |
+#    Select-Object "Time Start", "Time End", "Class Code"
+
+$ITSInstructors = $tableUpdate | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                                ($_."Class Code" -ilike "NET*") -or `
+                                                ($_."Class Code" -ilike "SEC*") -or `
+                                                ($_."Class Code" -ilike "FOR*") -or `
+                                                ($_."Class Code" -ilike "CSI*") -or `
+                                                ($_."Class Code" -ilike "DAY*") } `
+                               | Select-Object "Instructor" `
+                               | Sort-Object "Instructor" -unique
+$ITSInstructorsCorrect = $ITSInstructors | Where-Object {$_."Instructor" -notlike "*[0-9]*"}
+#$ITSInstructorsCorrect
+
+$tableUpdate | Where-Object {$_.Instructor -in $ITSInstructorsCorrect.Instructor} `
+| Group-Object "Instructor" | Select-Object -Property Count, Name | Sort-Object Count -Descending
